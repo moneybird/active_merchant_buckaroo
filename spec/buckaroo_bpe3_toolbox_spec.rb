dab86f49e7420c5cab86babfb706586e02c26cbb
Ivo <ivo@moneybird.com>
2017-11-07 19:32:47 +0100

    Do not unescape Buckaroo push parameters
    Summary:
    Fixes T7058
    
    In D447 is unescaping van parameters in Bucaroo pushes toegevoegd. Dit levert nu problemen op bij bedrijfsnamen waarin een `+` staat. Ik heb kort zitten rondklikken in [[https://graylog.moneybird.net/search?rangetype=relative&fields=message%2Csource&width=1494&highlightMessage=&relative=86400&q=action%3Apush_collecting_payments | Graylog]], maar ik kan eigenlijk geen voorbeelden vinden van de encoding met plusjes die in D447 beschreven wordt. Mogelijk heeft dat te maken met de refactor in D2684, of stuurt Buckaroo tegenwoordig gewoon andere pushes.
    
    Ik haal daarom de unescaping weg uit de gem.
    
    Test Plan:
    # Gooi de parameters uit een dead job in de gem. Zie een incorrecte signature.
    # Haal de unescaping weg, zie een correcte signature.
    
    Reviewers: #developers, oliver, yvonne, buildkite!
    
    Maniphest Tasks: T7058
    
    Differential Revision: https://phabricator.moneybird.net/D3422

--- spec/buckaroo_bpe3_toolbox_spec.rb
+++ spec/buckaroo_bpe3_toolbox_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper.rb"
 
 describe "Buckaroo BPE3 Toolbox" do
-  
+
   context "test all functions" do
-    
+
     before do
       @secretkey = "secretkey"
       @signature = "signature"
       @websitekey = "websitekey"
 
-      @params = { 
+      @params = {
         brq_amount: 1.23,
         brq_culture: "NL",
         brq_currency: "EUR",
@@ -22,11 +22,11 @@ describe "Buckaroo BPE3 Toolbox" do
         brq_websitekey: @websitekey,
       }
     end
-    
+
     it "should create good post data string" do
       ActiveMerchant::Billing::BuckarooBPE3Toolbox.create_post_data(@params, @signature).should == "brq_amount=1.23&brq_culture=NL&brq_currency=EUR&brq_description=MyInvoice&brq_invoicenumber=2013-0001&brq_payment_method=directdebit&brq_service_directdebit_action=Pay&brq_service_directdebit_customeraccountname=My+Name&brq_service_directdebit_customeraccountnumber=1234567&brq_websitekey=websitekey&brq_signature=signature"
     end
-    
+
     it "should create a valid signature" do
       ActiveMerchant::Billing::BuckarooBPE3Toolbox.create_signature(@params, @secretkey).should == "ed6646396c285a6f9285ca29af4e78f3535f2fdb"
     end
@@ -39,7 +39,7 @@ describe "Buckaroo BPE3 Toolbox" do
       params = { "brq_a" => 1, "brq_B" => 2, "brq_c" => 3 }
       ActiveMerchant::Billing::BuckarooBPE3Toolbox.sort_hash(params).should == [["brq_a", 1], ["brq_B", 2], ["brq_c", 3]]
     end
-    
+
     it "should sort the hash for signatures the right way (integer followed by string in string)" do
       # should sort hash this way:
       # [["brq_1_id", 1], ["brq_10_id", 1], ["brq_2_id", 1]]
@@ -48,10 +48,10 @@ describe "Buckaroo BPE3 Toolbox" do
       params = { "brq_1_id" => 1, "brq_10_id" => 1, "brq_2_id" => 1 }
       ActiveMerchant::Billing::BuckarooBPE3Toolbox.sort_hash(params).should == [["brq_1_id", 1], ["brq_10_id", 1], ["brq_2_id", 1]]
     end
-    
+
     it "should correctly check a signature" do
       str = "BRQ_AMOUNT=1.23&BRQ_APIRESULT=Pending&BRQ_CURRENCY=EUR&BRQ_INVOICENUMBER=2013-0001&BRQ_PAYMENT=1234567890ABCDEFGHIJKLMNOPQRSTUV&BRQ_PAYMENT_METHOD=directdebit&BRQ_STATUSCODE=791&BRQ_STATUSMESSAGE=Pending+processing&BRQ_TEST=false&BRQ_TIMESTAMP=2013-03-19+15%3a02%3a08&BRQ_TRANSACTIONS=1234567890ABCDEFGHIJKLMNOPQRSTUV&BRQ_SIGNATURE=dee5c83c666c9837051182d6d8866d2c1e5eb446"
-      
+
       params = Rack::Utils.parse_query(str)
       ActiveMerchant::Billing::BuckarooBPE3Toolbox.check_signature(params, @secretkey).should == true
       # make sure the BRQ_SIGNATURE is not deleted from the original hash
@@ -68,16 +68,5 @@ describe "Buckaroo BPE3 Toolbox" do
       result["BRQ_TEST"].should == nil
       result["brq_test"].should == "true"
     end
-
-    it "unescapes the string value when creating the signature" do
-      params_1 = { "brq_statusmessage"=>"Transaction+successfully+processed" }
-      params_2 = { "brq_statusmessage"=>"Transaction successfully processed" }
-
-      expect(ActiveMerchant::Billing::BuckarooBPE3Toolbox.create_signature(params_1, @secretkey)).to eq(
-        ActiveMerchant::Billing::BuckarooBPE3Toolbox.create_signature(params_2, @secretkey)
-      )
-    end
-
   end
-
 end
